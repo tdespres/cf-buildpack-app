23e0c9c6160ccd71848677299d8f304eaf9328ee
desprest <despres_theo@hotmail.com>
2018-10-18 14:52:50 +0200

    fc

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+//var cfenv = require("cfenv");
+//var appEnv = cfenv.getAppEnv();
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+	console.info("Listening on port: " + port);
+	console.log(process.versions);
+});
+
+app.get('/', function (req, res) {
+	res.type("text/html").status(200).send('<html><head></head><body><h1>Test with forced buildpack</h1><br />Node.js versions :<br /><pre>'+JSON.stringify(process.versions, undefined, 2)+'</pre></body></html>');
+});
